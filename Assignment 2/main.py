566dd8bc0c137378428aaf694a088765ccc8db26
Ashutosh <ashutosh29599@yahoo.co.in>
2023-04-11 21:40:54 -0500

    Completed assignment 2. Added report for assignment 1 & 2

--- Assignment 2/main.py
+++ Assignment 2/main.py
@@ -1,3 +1,12 @@
+'''
+Ashutosh Kumar
+UH ID 1854393
+
+COSC 6364 Advanced Numerical Analysis 
+Assignment 2
+'''
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -22,9 +31,7 @@ def gradient_descent(w_start, learning_rate, max_num_of_steps, tolerance):
     for i in range(max_num_of_steps):
         gradient = gradient_of_fun_at(w)
         w_next = w - (learning_rate * gradient)
-        # print(f"i = {i}: w = {w}, w_next = {w_next}")
 
-        # if np.linalg.norm(w - w_next) < tolerance:
         if ((w[0] - w_next[0]) < tolerance) and ((w[1] - w_next[1]) < tolerance):
             num_opt_steps = i + 1
             break
@@ -37,8 +44,9 @@ def gradient_descent(w_start, learning_rate, max_num_of_steps, tolerance):
     return w, f_min, num_opt_steps, path
 
 
-def plot(w1, w2, path, show):
-    w1grid, w2grid = np.meshgrid(np.linspace(-2,2,100), np.linspace(-2,2,100))
+def plot(path, learning_rate_lambda):
+    fig = plt.figure()
+    w1grid, w2grid = np.meshgrid(np.linspace(-2, 2.1, 100), np.linspace(-2, 2.1, 100))
     fgrid = fun([w1grid, w2grid])
     plt.contour(w1grid, w2grid, fgrid, levels=30)
 
@@ -48,25 +56,26 @@ def plot(w1, w2, path, show):
 
     plt.xlabel('w1')
     plt.ylabel('w2')
-    plt.title('Gradient Descent Trajectory')
+    plt.title(f'Gradient Descent Trajectory, \u03BB = {learning_rate_lambda}')
     
-    if show:
-        plt.show()
+    fig.savefig(f'plots/gd_lambda_{learning_rate_lambda}.png')
 
+    plt.show()
 
 
 if __name__ == "__main__":
     learning_rate_lambda = 0.1    # learning rate
     w_start = np.array([2,2])
     max_num_of_steps = 1000
-    termination_tolerance = 1e-7
+    termination_tolerance = 1e-6
 
     w_opt, f_min, num_opt_steps, path = gradient_descent(w_start, learning_rate_lambda, max_num_of_steps, termination_tolerance)
 
     print(f"Lambda (Learning rate) = {learning_rate_lambda}")
+    print(f"Termination tolerance = {termination_tolerance}")
     print(f"Optimal number of steps = {num_opt_steps}")
     print(f"Optimal w = {w_opt}")
     print(f"Minimum f = {f_min}")
 
-    plot(w_opt[0], w_opt[1], path, show=True)
+    plot(path, learning_rate_lambda)
 
